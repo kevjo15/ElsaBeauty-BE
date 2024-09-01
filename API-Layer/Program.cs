3b9ab67fb6f5c13f71c7b4b6868258011f2a0934
Kevin Jörgensen <114003395+kevjo15@users.noreply.github.com>
2024-09-01 23:16:41 +0200

    Establish database connection

--- API-Layer/Program.cs
+++ API-Layer/Program.cs
@@ -1,6 +1,24 @@
+using Domain_Layer.Models;
+using Infrastructure_Layer;
+using Infrastructure_Layer.Database;
+using Microsoft.AspNetCore.Identity;
+//using Application_Layer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Lägg till tjänster från Application_Layer
+//builder.Services.AddApplicationLayer();
+
+// Lägg till tjänster från Infrastructure_Layer
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+
+
+// Konfigurera Identity
+builder.Services.AddIdentity<UserModel, IdentityRole>()
+    .AddEntityFrameworkStores<ElsaBeautyDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Infrastructure-Layer/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure_Layer.Database;
+//using Infrastructure_Layer.Repositories.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure_Layer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ElsaBeautyDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+            //services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
